468fefdd5ba1ba8572528a4cb4f1bf7e505c3057
e210564e <devedelie@gmail.com>
2020-04-21 22:41:37 +0200

    - Added logic to check if internet is available - Added condition to redirect the user to the right activity depending on the status of the authentication - Added dialogBox to control wifi

--- app/src/main/java/com/elbaz/eliran/rappeltout/ui/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/elbaz/eliran/rappeltout/ui/activities/SplashScreenActivity.kt
@@ -1,27 +1,72 @@
 package com.elbaz.eliran.rappeltout.ui.activities
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.elbaz.eliran.rappeltout.R
-import kotlinx.coroutines.delay
+import com.elbaz.eliran.rappeltout.utils.Utils
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
-class SplashScreenActivity : AppCompatActivity() {
+open class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        intentLogin()
+
     }
 
-    private fun intentLogin(){
-//        waitForDelay()
-        val intent = Intent(this@SplashScreenActivity, LoginActivity::class.java)
-        startActivity(intent)
+    override fun onResume() {
+        super.onResume()
+        // Verify Network connectivity
+        if (!Utils.isInternetAvailable(this)) {
+            displayMobileDataSettingsDialog(this, this)
+        } else {
+        when (isCurrentUserLogged()){
+            true -> intentActivity(MainActivity::class.java)
+            else -> intentActivity(LoginActivity::class.java)  // Go to Login screen if logged Off
+        }
+        }
     }
 
 //    private suspend fun waitForDelay(){
 //        delay(500)
 //    }
+
+    // --------------------
+    // UTILS
+    // --------------------
+    protected fun isCurrentUserLogged(): Boolean? {
+        return this.getCurrentUser() != null
+    }
+
+    protected fun getCurrentUser(): FirebaseUser? {
+        return FirebaseAuth.getInstance().currentUser
+    }
+
+    open fun intentActivity(classname: Class<*>) {
+        val intent = Intent(this, classname)
+        startActivity(intent)
+    }
+
+    // --------------------
+    // NETWORK CONNECTIVITY
+    // --------------------
+    fun displayMobileDataSettingsDialog(activity: Activity?, context: Context): AlertDialog? {
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("No Internet")
+        builder.setMessage("Please enable network access")
+        builder.setPositiveButton("Continue") {
+                dialog, which -> finish()
+            val intent = Intent(context, this::class.java)
+            context.startActivity(intent)
+            dialog.cancel()
+        }
+        builder.show()
+        return builder.create()
+    }
 }
